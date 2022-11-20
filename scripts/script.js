6497fc7d14f9e8ead66499ab173fc69887d891fe
nonnodacciaio <nonnodacciaio@gmail.com>
2022-11-20 12:43:09 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+let computerOutput = document.getElementById("computer-output");
+let userOutput = document.getElementById("user-output")
+let rockInput = document.getElementById("rock-input").addEventListener("click", () => console.log("funziona"));
+let paperInput = document.getElementById("paper-input").addEventListener("click", () => console.log("funziona"));
+let scissorsInput = document.getElementById("scissors-input").addEventListener("click", () => console.log("funziona"));
